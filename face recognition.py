27fdb34d1e32ec0164a01e9ce5cb390169630fe8
Lee Jong Ann <50771738+LeeJongAnn@users.noreply.github.com>
2020-06-25 21:58:09 +0900

    Update face recognition.py

--- face recognition.py
+++ face recognition.py
@@ -1,6 +1,17 @@
 def face_recognition(img):
-  face_saver = detector(img,1) 
-  rectangular,shape = [],[]
+  dets = detector(img,1) 
+  if(lens(dets)==0): 
+    return np.empty(0), np.empty(0), np.empty(0)
+  rect,shape = [],[]
+  shapes_np= np.zeros((lens(dets),68,2)),dtype=int.numpy)
+  for k,d in enumerate(dets): 
+    rect=(d.left(),d.right(),d.top(),d.bottom())
+    rects.append(rect)
+    
+  
+  
+  
+  
   
 
 
