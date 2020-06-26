5ed8781a1fc9ace6885ec1675d04f858ff51cd9b
Lee Jong Ann <50771738+LeeJongAnn@users.noreply.github.com>
2020-06-26 12:49:38 +0900

    Update face recognition.py

--- face recognition.py
+++ face recognition.py
@@ -1,12 +1,22 @@
 def face_recognition(img): ## 얼굴 사각형을 인식하는 함수 업뎃
   dets = detector(img,1) 
+  
   if(lens(dets)==0): 
     return np.empty(0), np.empty(0), np.empty(0)
-  rect,shape = [],[]
+  rects,shapes = [],[]
   shapes_np= np.zeros((lens(dets),68,2)),dtype=int.numpy)
+  
   for k,d in enumerate(dets): 
     rect=(d.left(),d.right(),d.top(),d.bottom())
     rects.append(rect)
+    shape = sp(img,d)
+    for i in range(0,68): 
+      shapes_np[k][i] = (shape.part(i).x,shape.part(i).y) 
+     shapes.append(shape)
+      
+     return shapes,rects,shapes_np 
+    
+      
     
   
   
