5cc703580c9356eef79e54172ee65488b9dd328f
Lee Jong Ann <50771738+LeeJongAnn@users.noreply.github.com>
2020-06-30 14:00:55 +0900

    Update face recognition.py

--- face recognition.py
+++ face recognition.py
@@ -1,7 +1,14 @@
 import cv2
 import dlib
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.patheffects as path_effects
 
+
+detector = dlib.get_frontal_face_detector()
+sp = dlib.shape_predictor('models/shape_predictor_68_face_landmarks.dat')
+facerec = dlib.face_recognition_model_v1('models/dlib_face_recognition_resnet_model_v1.dat')
 
 def face_recognition(img): ## 얼굴 사각형을 인식하는 함수 업뎃
   dets = detector(img,1) 
